160edd23f3ca86e5f45d4058b7e6389855ccccd1
Ana Clara Brusamarello <clarasgore@gmail.com>
2023-07-17 21:55:08 -0300

    ajuste classe pai

--- src/main/java/com/br/projetointegrador/baraabbAPI/controller/AbstractController.java
+++ src/main/java/com/br/projetointegrador/baraabbAPI/controller/AbstractController.java
@@ -8,6 +8,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.br.projetointegrador.baraabbAPI.service.GenericService;
 
 import jakarta.validation.Valid;
 
+@CrossOrigin("*")
 public abstract class AbstractController<E extends AbstractEntity, DTO extends AbstractDTO> {
 
 	@Autowired
